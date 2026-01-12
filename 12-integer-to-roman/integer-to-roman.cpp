d0d0c656e988b8ba3f8defa37d792cb1d8493c9b
Syed Shah MD. Adib <48805976+4D1B@users.noreply.github.com>
2026-01-13 01:16:03 +0600

    Time: 87 ms (5.78%) | Memory: 13.9 MB (7.59%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -4,53 +4,16 @@ public:
     string intToRoman(int num) {
         int x = to_string(num).length()-1;
         string t = "";
-        for(int i=x;i>= 0 ;i--){
-
-            while( num != 0 ){
-                cout<< num << " "  <<endl;
-                if(num >= 1000){
-                    num = num-1000;
-                    t= t+ "M";
-                }else if(num >= 900){
-                    num = num-900;
-                    t= t+ "CM";
-                }else if(num >= 500){
-                    num = num-500;
-                    t= t+ "D";
-                }else if(num >= 400){
-                    num = num-400;
-                    t= t+ "CD";
-                }else if(num >= 100){
-                    num = num-100;
-                    t= t+ "C";
-                }else if(num >= 90){
-                    num = num-90;
-                    t= t+ "XC";
-                }
-                else if(num >= 50){
-                    num = num-50;
-                    t= t+ "L";
-                }else if(num >= 40){
-                    num = num-40;
-                    t= t+ "XL";
-                }
-                else if(num >= 10){
-                    num = num-10;
-                    t= t+ "X";
-                }else if(num >= 9){
-                    num = num-9;
-                    t= t+ "IX";
-                }
-                else if(num >= 5){
-                    num = num-5;
-                    t= t+ "V";
-                }else if(num >= 4){
-                    num = num-4;
-                    t= t+ "IV";
-                }else if(num >= 1){
-                    num = num-1;
-                    t= t+ "I";
-                }
+        const vector<pair<int, string>> romanMap = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"},  {90, "XC"},  {50, "L"},  {40, "XL"},
+            {10, "X"},   {9, "IX"},   {5, "V"},   {4, "IV"}, {1, "I"}
+        };
+        for (const auto& entry : romanMap) {
+            while (num >= entry.first) {
+                num -= entry.first;
+                t += entry.second;
+                cout << num << " " << endl; 
             }
         }
         return t;
