55b6d6d0fd4e85db950e06ce4aac9106f2db934c
Syed Shah MD. Adib <48805976+4D1B@users.noreply.github.com>
2026-01-17 18:18:44 +0600

    Time: 0 ms (100.00%) | Memory: 9.1 MB (34.29%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        size_t pos = haystack.find(needle);
+        if(pos == std::string::npos){
+            return -1;            
+        }else{ 
+            return pos;
+        }
+
+    }
+};
